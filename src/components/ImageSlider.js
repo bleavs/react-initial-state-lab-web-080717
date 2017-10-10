ee672778c97287fc4514b419011fd3f661333a97
bleavs <bleavitt@colgate.edu>
2017-10-10 10:20:54 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,52 @@
 // Bomb Component Code Goes Here
+
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render(){
+    return (
+      <p>I am on slide {this.state.currentSlideIndex}</p>
+    )
+  }
+}
+
+
+
+// // src/components/Address.js
+// import React from 'react';
+//
+// class Address extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       fullAddress: `${props.street}, ${props.city}`
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="address">
+//         {this.state.fullAddress}
+//       </div>
+//     );
+//   }
+// }
+//
+// export default Address;
+
+// class Address extends React.Component {
+//   render() {
+//     return (
+//       <div className="address">{this.props.street}, {this.props.city}</div>
+//     );
+//   }
+// }
